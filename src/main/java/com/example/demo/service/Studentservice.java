d69e4df9141102ad0155ad1a370f69d128c30a1f
bala-amypo <bala@amypo.in>
2025-12-17 05:11:50 +0000

    User commit

--- src/main/java/com/example/demo/service/Studentservice.java
+++ src/main/java/com/example/demo/service/Studentservice.java
@@ -5,6 +5,20 @@ import com.example.demo.entity.Studententity;
 
 @service
 public class Studentservice{
-    
+    private Map<Integer,Studententity>
+    details=new HashMap<>();
+    public Studententity savedata(Studententity st){
+        details.put(st.getid(),st);
+        return st;
+    }
+    public Studententity getStudent(int id){
+        return details.get(id);
+    }
+    public List<Studententity> getStudents(){
+        return details.values().stream().collect(Collectors.toList());
+    }
+    public Studententity updateStudent(int id,Studententity st){
+        if(details.containsKey(id))
+    }
 }
 
